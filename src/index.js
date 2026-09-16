6a6e4d7ff86a9f74908cf15ff3a64d03b2730c87
tommydear <tommyboy6633@gmail.com>
2026-09-16 11:39:39 +0100

    vercel correction

--- src/index.js
+++ src/index.js
@@ -38,6 +38,8 @@ app.get("/", (req, res) => {
     res.send("Hello World")
 })
 
+
+
 if(process.env.NODE_ENV !== "production") {
     app.listen(8080, () => {
         console.log("Server is running on port http://localhost:8080")
